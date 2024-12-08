24e750065709719bbf4836bdf4ef65e356ddefcd
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2024-12-08 23:06:24 +0100

    First fixes in main window

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require("node:path");
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
